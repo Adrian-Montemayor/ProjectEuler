207fc04e7fc44f7f384cf0688885de89dbdd01dd
Adrian-Montemayor <adrianmoncar@hotmail.com>
2020-06-20 15:48:23 -0500

    fixed fibonacci sequence and added even sum to result

--- Fibonacci/fibonacci.py
+++ Fibonacci/fibonacci.py
@@ -1,13 +1,20 @@
 def fibonacci(max_iterations):
     """
     Prints the fibonacci sequence until reach max iteration number
+    Prints the even sum, as well
     """
-    number1, number2 = 0,1
+    number1, number2 = 1,2
+    even_sum = 0
     print(number1, end=" ")
     for i in range (max_iterations - 1):
+        if(number1 > 4000000 or number2 > 4000000):
+            break
         number1, number2 = number2, number1 + number2
+        if number1 % 2 == 0:
+            even_sum += number1
         print(number1, end=" ")
-   
+    
+    print("\nEven Sum: " + str(even_sum))
 
 
 if __name__ == "__main__":
